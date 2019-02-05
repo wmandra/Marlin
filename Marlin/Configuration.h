642b852ba5d887a9e9822f6018884fa770ec6212
bill <wmandra@mac.com>
2019-02-05 09:46:56 -0500

    config changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -90,14 +90,14 @@
 #define INVERT_Z_DIR false
 
 #define X_HOME_DIR -1
-#define Y_HOME_DIR -1
+#define Y_HOME_DIR 1
 #define Z_HOME_DIR -1
 
 #define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
+//#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
-//#define USE_YMAX_PLUG
+#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
 #define X_MIN_ENDSTOP_INVERTING false // set to true to invert the logic of the endstop.
