07a34437ff4e49a51a5dae8823ef733cc8734af3
bill <wmandra@mac.com>
2019-02-11 16:30:44 -0500

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -48,7 +48,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2018-08-01"
+  #define STRING_DISTRIBUTION_DATE "2019-02-01"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
@@ -78,7 +78,7 @@
    * has a distinct Github fork— the Source Code URL should just be the main
    * Marlin repository.
    */
-  #define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+  #define SOURCE_CODE_URL "https://github.com/wmandra/Marlin"
 
   /**
    * Default generic printer UUID.
