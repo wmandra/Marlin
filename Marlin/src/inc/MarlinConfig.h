4782d4e3fb687bce85ac5eb5b73be251e14ecb76
bill <wmandra@mac.com>
2019-02-14 14:01:59 -0500

    Restructure code into sub-directories

---
+++ Marlin/src/inc/MarlinConfig.h
@@ -0,0 +1,48 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MARLIN_CONFIG_H
+#define MARLIN_CONFIG_H
+
+#include "../core/boards.h"
+#include "../core/macros.h"
+#include "Version.h"
+#include "../../Configuration.h"
+#include "Conditionals_LCD.h"
+#include "../core/drivers.h"
+#include "../../Configuration_adv.h"
+
+#if USE_MARLINSERIAL
+  #define HardwareSerial_h // trick to disable the standard HWserial
+#endif
+
+#include "../core/types.h"
+#include "../HAL/HAL.h"
+#include "../pins/pins.h"
+#include "Conditionals_post.h"
+#include "SanityCheck.h"
+#include "../core/enum.h"
+#include "../core/language.h"
+#include "../core/utility.h"
+#include "../core/serial.h"
+
+#endif // MARLIN_CONFIG_H
