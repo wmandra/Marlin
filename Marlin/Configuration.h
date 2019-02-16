6cbba3348b8c4d0c808bac0415143ff492fe0e1c
bill <wmandra@mac.com>
2019-02-16 15:56:05 -0500

    config fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,7 @@
 #define DEFAULT_TRAVEL_ACCELERATION   5000    // X, Y, Z acceleration for travel (non printing) moves
 
 #define DEFAULT_JERK  10.0
-#define DEFAULT_JUNCTION_DEVIATION  0.4*DEFAULT_JERK*DEFAULT_JERK/DEFAULT_ACCELERATION 
+#define DEFAULT_JUNCTION_DEVIATION  0.4*DEFAULT_JERK*DEFAULT_JERK/DEFAULT_ACCELERATION
 // 0.4 * Jerk^2 / Acceleration (used when NEW_JERK_CONTROL / JUNCTION_DEVIATION is enabled)
 // See http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
 
@@ -415,9 +415,9 @@
 #if ENABLED(TH3D_BED_THERMISTER)
   #define TEMP_SENSOR_BED 1
 #elif ENABLED(KEENOVO_TEMPSENSOR)
-  #define TEMP_SENSOR_0 11
+  #define TEMP_SENSOR_BED 11
 #else
-  #define TEMP_SENSOR_0 0
+  #define TEMP_SENSOR_BED 0
 #endif
 
 #if ENABLED(CASE_TEMP_ENABLE)
